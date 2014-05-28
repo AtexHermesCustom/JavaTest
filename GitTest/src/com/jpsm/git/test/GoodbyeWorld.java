e274e0e587221728e91313f7f7861bc7411d0541
jp <jmariano@atex.com>
2014-05-28 17:59:39 +0800

    update

--- GitTest/src/com/jpsm/git/test/GoodbyeWorld.java
+++ GitTest/src/com/jpsm/git/test/GoodbyeWorld.java
@@ -3,5 +3,6 @@ package com.jpsm.git.test;
 public class GoodbyeWorld {
 	public static void main(String args[]) {
 		System.out.println("Paalam");
+		System.out.println("pangalawa");
 	}
 }
--- GitTest/src/com/jpsm/git/test/HelloWorld.java
+++ GitTest/src/com/jpsm/git/test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.jpsm.git.test;
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Mabuhay");
+		System.out.println("pangalawa");
 	}
 }
